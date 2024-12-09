181dae4cc4860f22a7161cd6b395c6ff7ffaf2e3
Asuka Minato <i@asukaminato.eu.org>
2024-12-09 14:20:15 +0900

    fix send error

--- src/index.ts
+++ src/index.ts
@@ -135,7 +135,7 @@ export default {
 				if (!messageText.split(" ")[1]) {
 					const res = (await ctx.reply('请输入要查询的关键词'))!;
 					if (!res.ok) {
-						console.error(`Error sending message:`, JSON.stringify(await res.json()));
+						console.error(`Error sending message:`);
 					}
 					return new Response('ok');
 				}
