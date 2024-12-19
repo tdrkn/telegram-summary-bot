b0392dc9be4a1eb349a95b2a016aacf39ff554b5
Asuka Minato <i@asukaminato.eu.org>
2024-12-20 03:06:27 +0900

    fix size === 0 case

--- src/og.ts
+++ src/og.ts
@@ -29,6 +29,9 @@ export async function extractAllOGInfo(url: string): Promise<string> {
 		}
 		const rewriter = new HTMLRewriter().on('meta', new MetaHandler());
 		await rewriter.transform(response).text(); // 使用 await 处理 Promise
+		if(ogData.size === 0) {
+			return url;
+		}
 		let ret = "";
 		for (const [key, value] of ogData.entries()) {
 			ret += `${key}: ${value}\n`;
